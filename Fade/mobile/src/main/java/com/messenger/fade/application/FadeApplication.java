8da7784c985e80154b764637b1a94278f69cbb07
vevo-kevin-kawai <kevin.kawai@vevo.com>
2014-11-19 06:44:54 -0800

    Added api and user management

--- Fade/mobile/src/main/java/com/messenger/fade/application/FadeApplication.java
+++ Fade/mobile/src/main/java/com/messenger/fade/application/FadeApplication.java
@@ -1,19 +1,33 @@
 package com.messenger.fade.application;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
+import com.messenger.fade.R;
+import com.messenger.fade.model.User;
 import com.messenger.fade.util.BitmapLruCache;
+import com.messenger.fade.util.MLog;
+import com.messenger.fade.util.ThreadWrapper;
+
+import org.json.JSONObject;
 
 
-public class FadeApplication extends Application {
+public final class FadeApplication extends Application {
+
+    private static String SHARED_PREFS_ME_KEY = "me";
 
     private static RequestQueue requestQueue;
     private static ImageLoader imageLoader;
 
     private static FadeApplication instance;
+
+    private static SharedPreferences sharedPreferences;
+
+    private static User me;
 
 
     @Override
@@ -21,10 +35,63 @@ public class FadeApplication extends Application {
 
         super.onCreate();
 
+        ThreadWrapper.init();
+
+        MLog.setEnabled(getPackageName(), getResources().getBoolean(R.bool.is_logging_enabled));
+
         instance = this;
 
         requestQueue = Volley.newRequestQueue(this);
         imageLoader = new ImageLoader(requestQueue, new BitmapLruCache());
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        getMeFromPreferences();
+    }
+
+    private static User getMeFromPreferences() {
+        if (sharedPreferences.contains(SHARED_PREFS_ME_KEY)) {
+            try {
+                me = User.from(new JSONObject(sharedPreferences.getString(SHARED_PREFS_ME_KEY, null)));
+            } catch (final Exception e) {
+
+            }
+        }
+        return me;
+    }
+
+    /**
+     * Invoke after authenticating or creating a new account
+     *
+     * @param user
+     */
+    public static void setMe(final User user) {
+        me = user;
+        sharedPreferences.edit().putString(SHARED_PREFS_ME_KEY, user.toJSON().toString()).apply();
+    }
+
+    /**
+     * Invoke when user logs out
+     */
+    public static void removeMe() {
+        me = null;
+        sharedPreferences.edit().remove(SHARED_PREFS_ME_KEY).apply();
+    }
+
+    /**
+     * Returns myself if logged id, null otherwise.
+     *
+     * @return
+     */
+    public static User me() {
+        if (me != null) {
+            return me;
+        }
+        return getMeFromPreferences();
+    }
+
+    public static SharedPreferences getSharedPreferences() {
+        return sharedPreferences;
     }
 
     public static FadeApplication getInstance() {
--- Fade/mobile/src/main/java/com/messenger/fade/rest/FadePostRequest.java
+++ Fade/mobile/src/main/java/com/messenger/fade/rest/FadePostRequest.java
@@ -1,20 +1,43 @@
 package com.messenger.fade.rest;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.messenger.fade.util.FadeLog;
+import com.android.volley.toolbox.StringRequest;
+import com.messenger.fade.util.NetworkUtil;
 
-import org.json.JSONObject;
+import java.util.Map;
 
-public class FadePostRequest extends JsonObjectRequest {
+public class FadePostRequest extends StringRequest {
 
     private static final String TAG = FadePostRequest.class.getSimpleName();
 
-    public FadePostRequest(String url, JSONObject jsonRequest, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+    private Map<String,String> mParams;
 
-        super(Request.Method.POST, url, jsonRequest, listener, errorListener);
+    public FadePostRequest(final String url, final Map<String,String> params, final Response.Listener<String> responder, final Response.ErrorListener errorListener) {
 
-        FadeLog.d(TAG, "POST " + url + "  " + jsonRequest.toString());
+        super(Request.Method.POST, url, responder, errorListener);
+        mParams = params;
+
+        NetworkUtil.logToCurlRequest(this);
+
     }
+
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+
+        return mParams;
+    }
+
+    /*
+     * we are not using header session token authorization
+     * yes, fade is totally insecure for now lolz
+     */
+//    @Override
+//    public HashMap<String, String> getHeaders() {
+//        HashMap<String, String> params = new HashMap<String, String>();
+//        params.put(ApiV2.AUTH_HEADER, ApiV2.AUTH_HEADER_BEARER + User.getSessionToken());
+//        return params;
+//    }
 }
